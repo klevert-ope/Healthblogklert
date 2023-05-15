742235500df2eb32bcd0de04941c05e2c713b6bb
Klevert Opee <klevertope@gmail.com>
2023-05-15 21:38:53 +0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
-import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://healthblogklert.vercel.app/',
-  integrations: [tailwind(), sitemap(), compress(), robotsTxt()]
+  integrations: [tailwind(), compress()]
 });
